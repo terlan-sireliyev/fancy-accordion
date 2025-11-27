182e5eda71af72c05292528d68f3b8d073ee7090
terlan-sireliyev <sireliyev95@list.ru>
2025-11-27 17:21:17 +0300

    accordion menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  const menus = document.querySelectorAll(".menu-item");
+
+      menus.forEach((menu) => {
+        menu.addEventListener("click", () => {
+          menus.forEach((m) => {
+            if (m !== menu) m.classList.remove("active");
+          });
+          menu.classList.toggle("active");
+        });
+      });
